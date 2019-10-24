0e86982f0509b14b7aaae3641bd6fbe7bc59bee8
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2019-10-24 09:06:19 -0300

    ajustes algoritmo qual sua idade

--- assets/scripts/verificador-de-idade.js
+++ assets/scripts/verificador-de-idade.js
@@ -1,13 +1,16 @@
 var btnCalcula = document.getElementById('btnCalcula');
 btnCalcula.addEventListener('click', function () {
+    
     // pegando respostas do usuário
     var ano = new Date()
     var ano_atual = ano.getFullYear()
-    var idade = ano_atual - document.getElementById('ano_nascimento').value
+    var idade = Number(ano_atual) - Number(document.getElementById('ano_nascimento').value)
     var sexo = document.querySelector('input[name="pSexo"]:checked').value;
+    
     //pegando as referências dos campos que vão ser alterados
     var descricao = document.getElementById('descricaoPessoa');
     var fotoPessoa = document.getElementById('fotoPessoa');
+
     //filtrando o resultado
     if( document.getElementById('ano_nascimento').value > ano.getFullYear() || document.getElementById('ano_nascimento').value == '' ){
         alert(`Ano inserido invalido ${document.getElementById('ano_nascimento').value}`);
@@ -15,30 +18,30 @@ btnCalcula.addEventListener('click', function () {
         fotoPessoa.style.width = '250px';
         fotoPessoa.style.height = '250px';
         if( idade == 0 ){
-        descricao.innerText = 'Você é um bebê.'
-        fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/bebe.jpg)'
+            descricao.innerText = 'Você é um bebê.';
+            fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/bebe.jpg)';
         }
         else if( idade < 24 ){
             if(sexo == 'M' ){
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/homem-adolescente.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/homem-adolescente.jpg)';
             }else{  
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/mulher-adolescente.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/mulher-adolescente.jpg)';
             }
-            descricao.innerText = 'Você é um adolescente ou está perto de ser.'
+            descricao.innerText = 'Você é um adolescente ou está perto de ser.';
         }else if( idade < 59 ){
             if(sexo == 'M' ){
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/homem-adulto.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/homem-adulto.jpg)';
             }else{  
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/mulher-adulta.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/mulher-adulta.jpg)';
             }
-            descricao.innerText = 'Você é um adulto.'
+            descricao.innerText = 'Você é um adulto.';
         }else{
             if(sexo == 'M' ){
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/idoso.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/idoso.jpg)';
             }else{
-                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/idosa.jpg)'
+                fotoPessoa.style.backgroundImage = 'url(assets/imagens/exerc-idade/idosa.jpg)';
             }   
-            descricao.innerText = 'Você passou dos 59 aproveita para investir em você.'
+            descricao.innerText = 'Você passou dos 59 aproveita para investir em você.';
         }
     }   
 } )
